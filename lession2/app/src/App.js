40f9981242ffd0ac382a08001f8201f65723503a
ngocchientk95 <carry.b0y0895@gmail.com>
2025-02-12 22:35:29 +0700

    study-JS

---
+++ lession2/app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+function App() {
+  return (
+      <div className="App">
+          <h4>Browser's details: {navigator.userAgent}</h4>
+      </div>
+  );
+}
+
+export default App;
